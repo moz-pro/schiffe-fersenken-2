d470711646a453a356fbd400f558cb5d811a6003
moz-pro <mozpro@outlook.de>
2024-12-05 13:49:50 +0100

    anfang mit bugs

--- main.ts
+++ main.ts
@@ -1,3 +1,64 @@
+enum RadioMessage {
+    message1 = 49434,
+    hello2 = 22932,
+    hello = 49337,
+    hallo = 324
+}
+radio.onReceivedMessage(RadioMessage.hallo, function () {
+    basic.showIcon(IconNames.Yes)
+    basic.pause(2000)
+    schiff1 = game.createSprite(0, 0)
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            schiff1.change(LedSpriteProperty.X, 1)
+        } else {
+            if (input.buttonIsPressed(Button.B)) {
+                schiff1.change(LedSpriteProperty.X, -1)
+            }
+        }
+    }
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            schiff1.change(LedSpriteProperty.Y, 1)
+        } else {
+            if (input.buttonIsPressed(Button.B)) {
+                schiff1.change(LedSpriteProperty.Y, -1)
+            }
+        }
+    }
+    Lklein = [game.createSprite(0, 1), game.createSprite(0, 0), game.createSprite(1, 0)]
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            Lklein.turn(Direction.Left, 45)
+        } else {
+            if (input.buttonIsPressed(Button.B)) {
+                Lklein.turn(Direction.Right, 45)
+            }
+        }
+    }
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            schiff1.change(LedSpriteProperty.X, 1)
+        } else {
+            if (input.buttonIsPressed(Button.B)) {
+                schiff1.change(LedSpriteProperty.X, -1)
+            }
+        }
+    }
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            schiff1.change(LedSpriteProperty.Y, 1)
+        } else {
+            if (input.buttonIsPressed(Button.B)) {
+                schiff1.change(LedSpriteProperty.Y, -1)
+            }
+        }
+    }
+})
+let Lklein: game.LedSprite[] = []
+let schiff1: game.LedSprite = null
+radio.setGroup(1)
+radio.sendMessage(RadioMessage.hello2)
 basic.forever(function () {
 	
 })
